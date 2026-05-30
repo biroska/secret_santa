bba4817b9a2d174863510901abf04ab47e0e30bf
Aimberê Galdino <biroska@gmail.com>
2026-05-30 14:32:56 -0300

    Fix deploy no Android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,8 @@ plugins {
 android {
     namespace = "br.com.galdino.secret_santa"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+//    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
